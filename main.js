b89eab9202d6ca95d20b6888124bef8bf270e592
Arela-Cristina <instagrandine@gmail.com>
2024-10-23 16:29:56 +0200

    refactor naming and conditions

--- main.js
+++ main.js
@@ -3,12 +3,9 @@
 //elemento form
 const paxForm = document.getElementById('paxForm');
 
-
-
-
-//rate to travel (0.21 € by km)
-const kmRate = 0.21;
-let fullRate = kmNum * kmRate;
+// //rate to travel (0.21 € by km)
+// const kmRate = 0.21;
+// let fullRate = kmNum * kmRate;
 
 //evento 
 paxForm.addEventListener('submit', function (event) {
@@ -21,8 +18,28 @@ paxForm.addEventListener('submit', function (event) {
     //elemento km
     let kmNum = document.getElementById('kmNum').value;
 
-    //elemento age
-    let age = document.getElementById('age').value;
+    //elemento age dropdown
+    let age = document.getElementById('age');
+    let ageOption = age.value;
 
-    console.log(`name: ${name} - kmNum: ${kmNum} - age: ${age}`)
+    console.log(`name: ${name} - kmNum: ${kmNum} - age: ${ageOption}`);
+
+    //rate to travel (0.21 € by km)
+    const kmRate = 0.21;
+    let fullRate = kmNum * kmRate;
+
+    //impose a 0% discount   as a fullRate
+    let discount = fullRate.toFixed(2);
+    console.log(`This is full rate $  ${fullRate}`);
+    //impose a 20% discount for minor passenger  
+    if (ageOption === 'Minor') {
+        discount = fullRate * (20 / 100);
+        discount = (fullRate - discount).toFixed(2);
+        console.log(`Apply 20% discount for minor pax $ ${discount}`);
+        //impose a 40% discount for passengers over 65 years old
+    } else if (ageOption === 'Over65') {
+        discount = fullRate * (40 / 100);
+        discount = (fullRate - discount).toFixed(2);
+        console.log(`Apply 40% discount for pax over 65 $ ${discount}`)
+    }
 });
